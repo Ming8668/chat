5fcafa31527638369437d81879d59499e61ff534
Ming8668 <mike86.chan@gmail.com>
2024-04-29 21:33:07 +0800

    done with r1.py

---
+++ r1.py
@@ -0,0 +1,37 @@
+
+def read_file(filename):
+	lines = []
+	with open(filename, 'r', encoding='utf-8-sig') as f:
+		for line in f:
+			lines.append(line.strip())
+	return lines
+
+
+def conver(lines):
+	new = []
+	person = None # 避免第一行為空，設一預設值(空值) # 能還是有預告變數
+	for line in lines:
+		if line == 'Allen':
+			person = 'Allen'
+			continue
+		elif line == 'Tom':
+			person = 'Tom'
+			continue
+		if person:
+			new.append(person + ': ' + line)
+	return new
+
+
+def write_file(filename, lines):
+	with open(filename, 'w') as f:
+		for line in lines:
+			f.write(line + '\n')
+
+
+def main():
+	lines = read_file('input.txt')
+	lines = conver(lines)
+	write_file('output.txt', lines)
+
+
+main()
